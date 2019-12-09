c2a04a63407a375a8b3c64bfdc602151b57997fa
jiangji1 <774133054@qq.com>
2019-12-09 18:29:09 +0800

    深入浅出node，1.commonjs

---
+++ commonjs/a.js
@@ -0,0 +1,12 @@
+// module.exports = 2
+
+Object.defineProperty(module, "exports", {
+  enumerable: false,
+  configurable: false,
+  writable: false,
+  value: "exports"
+});
+
+// delete module.exports
+
+exports.a = 'a1'
---
+++ commonjs/index.js
@@ -0,0 +1,15 @@
+// console.log(module.paths)
+
+// console.log(require('./a'))
+// console.log(require('./a.js'))
+
+// console.dir(require)
+// console.dir(module)
+// console.dir(exports)
+// console.log(exports === module.exports) // true
+
+// require('vm')
+
+// require('./a.js.js')
+// require('./a.js.js')
+
